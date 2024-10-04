868bf37f844d539dbb9c07d0f9848092db732d65
Alejandro <gar231136@uvg.egu.gt>
2024-10-04 06:44:25 -0600

    Create AppBar

--- app/src/main/java/com/uvg/edu/gt/uvghorasbeca/MainActivity.kt
+++ app/src/main/java/com/uvg/edu/gt/uvghorasbeca/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.uvg.edu.gt.uvghorasbeca.ui.theme.UVGHorasBecaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UVGHorasBecaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    topAppBar(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun topAppBar(modifier: Modifier = Modifier) {
+    Row(
         modifier = modifier
-    )
-}
+            .background(color = Color(0xFF27C24C))
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+            IconButton(onClick = { /* Acción al hacer clic en el ícono */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menú",
+                    tint = Color.White
+                )
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UVGHorasBecaTheme {
-        Greeting("Android")
     }
 }
