6dcaaf363bfd22a6511fd5f8238c224d119bfbf9
José Antonio Mérida <joseameridac@gmail.com>
2024-10-06 20:37:45 -0600

    Modify Colors

--- app/src/main/java/com/uvg/edu/gt/uvghorasbeca/navigation/NavigationState.kt
+++ app/src/main/java/com/uvg/edu/gt/uvghorasbeca/navigation/NavigationState.kt
@@ -3,4 +3,5 @@ package com.uvg.edu.gt.uvghorasbeca.navigation
  sealed class NavigationState(val route: String){
      object WelcomeScreen : NavigationState("WelcomeScreen")
      object LoginScreen : NavigationState("LoginScreen")
+     object AdminScreen : NavigationState("name")
  }
